b1b197aa1a37a9d6699009586315b29531ea576f
Amaia Iparragirre <40216412+aiparragirre@users.noreply.github.com>
2023-01-11 14:12:40 +0100

    Add files via upload

---
+++ usage-package.R
@@ -0,0 +1,113 @@
+
+# Adibidea
+# -------------------------------------------------------------------------
+
+setwd("/Users/amaiaiparragirre/SynologyDrive/Lana/Tesia/2022-07 Estantzia NZ/Ikerkuntza/Programazioa")
+
+library(wlasso)
+
+# Example -----------------------------------------------------------------
+
+# Linear regression
+
+load("Simulations/res_linear_scenario2.RData")
+sample <- res_linear_scenario2$sample_100$data
+rm(res_linear_scenario2)
+
+mcv <- wlasso(data = sample, col.y = "y", col.x = 1:50, family = "gaussian", 
+              cluster = "cluster", strata = "strata", weights = "weights",
+              method = "cv", k=10, R=20, seed = 100)
+plot(mcv)
+coef(mcv$model.min)
+
+mboot <- wlasso(data = sample, col.y = "y", col.x = 1:50, family = "gaussian", 
+                cluster = "cluster", strata = "strata", weights = "weights",
+                method = "bootstrap", B=200, seed = 100)
+plot(mboot)
+coef(mboot$model.min)
+
+mjkn <- wlasso(data = sample, col.y = "y", col.x = 1:50, family = "gaussian", 
+               cluster = "cluster", strata = "strata", weights = "weights",
+               method = "JKn", seed = 100)
+plot(mjkn)
+coef(mjkn$model.min)
+
+mbrr <- wlasso(data = sample, col.y = "y", col.x = 1:50, family = "gaussian", 
+               cluster = "cluster", strata = "strata", weights = "weights",
+               method = "BRR", seed = 100)
+plot(mbrr)
+coef(mbrr$model.min)
+
+msplit <- wlasso(data = sample, col.y = "y", col.x = 1:50, family = "gaussian", 
+                 cluster = "cluster", strata = "strata", weights = "weights",
+                 method = "split", train.prob = 0.7, method.split = "cv", R=100,
+                 seed = 100)
+plot(msplit)
+coef(mbrr$model.min)
+
+msplit <- wlasso(data = sample, col.y = "y", col.x = 1:50, family = "gaussian", 
+                 cluster = "cluster", strata = "strata", weights = "weights",
+                 method = "split", train.prob = 0.7, method.split = "bootstrap", R=100,
+                 seed = 100)
+plot(msplit)
+coef(msplit$model.min)
+
+mextra <- wlasso(data = sample, col.y = "y", col.x = 1:50, family = "gaussian", 
+                 cluster = "cluster", strata = "strata", weights = "weights",
+                 method = "extrapolation", train.prob = 0.7, R=100,
+                 seed = 100)
+plot(mextra)
+coef(mextra$model.min)
+
+
+# Logistic regression
+
+load("Simulations/res_logistic_scenario2.RData")
+sample <- res_logistic_scenario2$sample_100$data
+rm(res_logistic_scenario2)
+
+mcv <- wlasso(data = sample, col.y = "y", col.x = 1:50, family = "binomial", 
+              cluster = "cluster", strata = "strata", weights = "weights",
+              method = "cv", k=10, R=20, seed = 100)
+plot(mcv)
+coef(mcv$model.min)
+
+mboot <- wlasso(data = sample, col.y = "y", col.x = 1:50, family = "binomial", 
+                cluster = "cluster", strata = "strata", weights = "weights",
+                method = "bootstrap", B=200, seed = 100)
+plot(mboot)
+coef(mboot$model.min)
+
+mjkn <- wlasso(data = sample, col.y = "y", col.x = 1:50, family = "binomial", 
+               cluster = "cluster", strata = "strata", weights = "weights",
+               method = "JKn", seed = 100)
+plot(mjkn)
+coef(mjkn$model.min)
+
+mbrr <- wlasso(data = sample, col.y = "y", col.x = 1:50, family = "binomial", 
+               cluster = "cluster", strata = "strata", weights = "weights",
+               method = "BRR", seed = 100)
+plot(mbrr)
+coef(mbrr$model.min)
+
+msplit <- wlasso(data = sample, col.y = "y", col.x = 1:50, family = "binomial", 
+                 cluster = "cluster", strata = "strata", weights = "weights",
+                 method = "split", train.prob = 0.7, method.split = "cv", R=100,
+                 seed = 100)
+plot(msplit)
+coef(mbrr$model.min)
+
+msplit <- wlasso(data = sample, col.y = "y", col.x = 1:50, family = "binomial", 
+                 cluster = "cluster", strata = "strata", weights = "weights",
+                 method = "split", train.prob = 0.7, method.split = "bootstrap", R=100,
+                 seed = 100)
+plot(msplit)
+coef(msplit$model.min)
+
+mextra <- wlasso(data = sample, col.y = "y", col.x = 1:50, family = "binomial", 
+                 cluster = "cluster", strata = "strata", weights = "weights",
+                 method = "extrapolation", train.prob = 0.7, R=100,
+                 seed = 100)
+plot(mextra)
+coef(mextra$model.min)
+
