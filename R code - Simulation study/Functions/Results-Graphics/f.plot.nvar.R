08d64d6a677505297cddba428271b56dbd12bba4
Amaia Iparragirre <40216412+aiparragirre@users.noreply.github.com>
2023-02-15 20:33:45 +0100

    Update f.plot.nvar.R

--- R code - Simulation study/Functions/Results-Graphics/f.plot.nvar.R
+++ R code - Simulation study/Functions/Results-Graphics/f.plot.nvar.R
@@ -1,14 +1,9 @@
 f.plot.nvar <- function(res, obj.name, ylim = NULL, opacity = 0.25,
                         l.width = 8, l.height = 5, scenario = NULL,
                         method.ind, method.names, colours = "lightgray"){  
-  pdf(file = paste0("Graphics/zeros/nvar-",obj.name,".pdf"), width = l.width, height=l.height)
-  #par(mar = c(8, 4, 4, 2) + 0.1)
   
-  #if(is.null(scenario)){
-  #  title <- "Number of variables"
-  #} else {
-  #  title <- paste0(scenario, ": Number of variables")
-  #}
+  # Number of variables in the final models
+  pdf(file = paste0("Graphics/zeros/nvar-",obj.name,".pdf"), width = l.width, height=l.height)
   
   l.zeros <- list()
   l.zeros[["True"]] <- res[["pop.estimated.nonzeros"]]
@@ -20,21 +15,16 @@ f.plot.nvar <- function(res, obj.name, ylim = NULL, opacity = 0.25,
   abline(v = 1.5, col = "gray", lty = 1, lwd=2)
   dev.off()
   
-  pdf(file = paste0("Graphics/zeros/diff-nvar-",obj.name,".pdf"), width = l.width, height=l.height)
-  #par(mar = c(8, 4, 4, 2) + 0.1)
   
-  #if(is.null(scenario)){
-  #  title <- "Difference in number of variables"
-  #} else {
-  #  title <- paste0(scenario, ": Difference in number of variables")
-  #}
+  # Difference between the number of variables between the fitted model and the "true" number of variables
+  pdf(file = paste0("Graphics/zeros/diff-nvar-",obj.name,".pdf"), width = l.width, height=l.height)
   
   l.diffzeros <- list()
   for(m in method.ind){
     l.diffzeros[[paste0("method_",m)]] <- res[[paste0("method_",m)]][["estimated.nonzeros"]] - res[["pop.estimated.nonzeros"]]
   }
   names(l.diffzeros) <- method.names
-  boxplot(l.diffzeros, las=2, cex.axis = 1, main = scenario, col = colours, ylab = "Difference in number of variables")
+  boxplot(l.diffzeros, las=2, cex.axis = 1, main = scenario, col = colours[-1], ylab = "Difference in number of variables")
   abline(h=0, col="red")
   dev.off()
   
