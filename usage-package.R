5ba8674bf573cc53feb5b8a48d17e077891cf8ca
Amaia Iparragirre <40216412+aiparragirre@users.noreply.github.com>
2023-02-23 12:46:36 +0100

    Update usage-package.R

--- usage-package.R
+++ usage-package.R
@@ -5,47 +5,54 @@
 
 load("example-data/sample_G.RData")
 
+# --> Design-based cross-validation (dCV): k=10 folds and R=1 replicates
 mcv <- wlasso(data = sample_G, col.y = "y", col.x = 1:50, family = "gaussian", 
               cluster = "cluster", strata = "strata", weights = "weights",
-              method = "dCV", k=10, R=20, seed = 100)
+              method = "dCV", k=10, R=1, seed = 100)
 plot(mcv)
 coef(mcv$model.min)
 
+# --> Bootstrap (ah-1 PSUs, "subbootstrap")
 mboot <- wlasso(data = sample_G, col.y = "y", col.x = 1:50, family = "gaussian", 
                 cluster = "cluster", strata = "strata", weights = "weights",
-                method = "bootstrap", B=200, seed = 100)
+                method = "subbootstrap", B=200, seed = 100)
 plot(mboot)
 coef(mboot$model.min)
 
+# --> Jacknife Repeated Replication (JKn)
 mjkn <- wlasso(data = sample_G, col.y = "y", col.x = 1:50, family = "gaussian", 
                cluster = "cluster", strata = "strata", weights = "weights",
                method = "JKn", seed = 100)
 plot(mjkn)
 coef(mjkn$model.min)
 
+# --> Balanced Repeated Replication (BRR)
 mbrr <- wlasso(data = sample_G, col.y = "y", col.x = 1:50, family = "gaussian", 
                cluster = "cluster", strata = "strata", weights = "weights",
                method = "BRR", seed = 100)
 plot(mbrr)
 coef(mbrr$model.min)
 
+# --> Split-sample Repeated Replication with cross-validated replicate weights (R=20)
 msplit <- wlasso(data = sample_G, col.y = "y", col.x = 1:50, family = "gaussian", 
                  cluster = "cluster", strata = "strata", weights = "weights",
-                 method = "split", train.prob = 0.7, method.split = "cv", R=100,
+                 method = "split", train.prob = 0.7, method.split = "cv", R=20,
                  seed = 100)
 plot(msplit)
 coef(mbrr$model.min)
 
+# --> Split-sample Repeated Replication with bootstrap replicate weights (R=20)
 msplit <- wlasso(data = sample_G, col.y = "y", col.x = 1:50, family = "gaussian", 
                  cluster = "cluster", strata = "strata", weights = "weights",
-                 method = "split", train.prob = 0.7, method.split = "bootstrap", R=100,
+                 method = "split", train.prob = 0.7, method.split = "bootstrap", R=20,
                  seed = 100)
 plot(msplit)
 coef(msplit$model.min)
 
+# --> Extrapolation (R=20)
 mextra <- wlasso(data = sample_G, col.y = "y", col.x = 1:50, family = "gaussian", 
                  cluster = "cluster", strata = "strata", weights = "weights",
-                 method = "extrapolation", train.prob = 0.7, R=100,
+                 method = "extrapolation", train.prob = 0.7, R=20,
                  seed = 100)
 plot(mextra)
 coef(mextra$model.min)
@@ -57,7 +64,7 @@ load("example-data/sample_B.RData")
 
 mcv <- wlasso(data = sample_B, col.y = "y", col.x = 1:50, family = "binomial", 
               cluster = "cluster", strata = "strata", weights = "weights",
-              method = "cv", k=10, R=20, seed = 100)
+              method = "dCV", k=10, R=1, seed = 100)
 plot(mcv)
 coef(mcv$model.min)
 
@@ -81,21 +88,21 @@ coef(mbrr$model.min)
 
 msplit <- wlasso(data = sample_B, col.y = "y", col.x = 1:50, family = "binomial", 
                  cluster = "cluster", strata = "strata", weights = "weights",
-                 method = "split", train.prob = 0.7, method.split = "cv", R=100,
+                 method = "split", train.prob = 0.7, method.split = "cv", R=20,
                  seed = 100)
 plot(msplit)
 coef(mbrr$model.min)
 
 msplit <- wlasso(data = sample_B, col.y = "y", col.x = 1:50, family = "binomial", 
                  cluster = "cluster", strata = "strata", weights = "weights",
-                 method = "split", train.prob = 0.7, method.split = "bootstrap", R=100,
+                 method = "split", train.prob = 0.7, method.split = "bootstrap", R=20,
                  seed = 100)
 plot(msplit)
 coef(msplit$model.min)
 
 mextra <- wlasso(data = sample_B, col.y = "y", col.x = 1:50, family = "binomial", 
                  cluster = "cluster", strata = "strata", weights = "weights",
-                 method = "extrapolation", train.prob = 0.7, R=100,
+                 method = "extrapolation", train.prob = 0.7, R=20,
                  seed = 100)
 plot(mextra)
 coef(mextra$model.min)
